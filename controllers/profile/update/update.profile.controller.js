17610dd782dd9fe6b2f75a74f5d2b495885904bc
Manpreet-Tiwari <105644197+Manpreet-Tiwari@users.noreply.github.com>
2023-06-13 11:52:13 +0530

    profile update password Controller API

--- controllers/profile/update/update.profile.controller.js
+++ controllers/profile/update/update.profile.controller.js
@@ -1,67 +1,63 @@
-const { default: mongoose } = require("mongoose");
-const User = require("../../../models/user/user.model");
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const User = require("../../../models/user/user.model");
 
-const updateUserProfile = (req, res, next) => {
-    try {
-        let data = req.body;
-        let Id = req.user._id;
-        if (!Id) {
-            let error = new Error('Unauthorized User');
-            error.status = 401;
-            throw error;
-        }
-        else {
-            try {
-                if (data.password) {
-                    bcrypt.hash(data.password, 10, async (err, encrypted) => {
-                        if (err) {
-                            err.message = 'Something went wrong while hashing password';
-                            throw err;
-                        }
-                        else {
-                            data = { ...data, password: encrypted };
-                        }
-                    });
-                }
-            }
-            catch (err) {
-                if (!err.message) err.message = 'Error while updating Profile';
-                if (!err.status) err.status = 400;
-                throw err; 
-            }
-            finally {
-                User.updateOne({ _id: mongoose.Types.ObjectId(Id) }, { $set: data }).then((result) => {
-                    if (result.acknowledged === true) {
-                        if (result.modifiedCount > 0) {
-                            res.status(200).send({
-                                "Message": "Profile Updated Successfully"
-                            })
-                        }
-                        else {
-                            let error = new Error('Error while updating profile');
-                            error.status = 400;
-                            throw error;
-                        }
-                    }
-                    else {
-                        let error = new Error('Invalid User Id');
-                        error.status = 400;
-                        throw error;
-                    }
-                }).catch((err) => {
-                    if (!err.message) err.message = "Error while updating Profile";
-                    if (!err.status) err.status = 400;
-                    next(err);
+const UpdateUserWithGivenData = (data, next) => {
+    User.updateOne({ _id: mongoose.Types.ObjectId(data._id) }, { $set: data }).then((result) => {
+        console.log(result);
+        if (result.acknowledged === true) {
+            if (result.modifiedCount > 0) {
+                res.status(202).send({
+                    "Message": "User Updated Successfully!"
                 })
             }
+            else {
+                let error = new Error('Error while updating user');
+                error.status = 400;
+                throw error;
+            }
+        }
+        else {
+            let error = new Error('Something went wrong while updating user');
+            error.status = 400;
+            throw error;
+        }
+    }).catch((err) => {
+        if (!err.message) err.message = 'Something went wrong while updating user!';
+        if (!err.status) err.status = 400;
+        next(err);
+    })
+}
+
+const updateUserProfile = (req, res, next) => {
+    let data = req.body;
+    console.log(data);
+    try {
+
+        if (data.password) {
+            bcrypt.hash(data.password, 10, async (err, encrypted) => {
+                if (err) {
+                    err.message = 'Something Went wrong while hashing the password';
+                    throw err;
+                }
+                else {
+                    data = { ...data, password: encrypted };
+                    UpdateUserWithGivenData(data, next);
+                }
+            })
+        }
+        else {
+            UpdateUserWithGivenData(data, next);
         }
     }
     catch (err) {
-        if (!err.message) err.message = 'Error while updating Profile';
+        if (!err.message) err.message = 'Error while updating User';
         if (!err.status) err.status = 400;
         throw err;
     }
 }
+
+
+
 
 module.exports = updateUserProfile;
